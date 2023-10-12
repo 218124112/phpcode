ea90f58569a70c071af19d48ff72880e75692616
dylanDA23 <83214778+dylanDA23@users.noreply.github.com>
2023-10-12 17:50:15 +0200

    nothing

--- index.php
+++ index.php
@@ -1,29 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecommerce website using php and mysql</title>
-    <!-- boostrap css link -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- font awsome link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- css file -->
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Electronics Website</title>
+  <!-- bootstrap css link -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+  crossorigin="anonymous">
+  <!-- font awesome link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" 
+  integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+  crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+  <!-- css file -->
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- navbar -->
-    <div class="containner-fluid">
-        <!-- first child -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-info">
-        <img src="./resources/gorillaTechnologies.png" alt="" class="logo">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" 
-  data-target="#navbardropdown" aria-controls="navbardropdown" 
-  aria-expanded="false" aria-label="Toggle navigation">
+  <!-- navbar -->
+  <div class="container-fluid p-0">
+    <!-- first child -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <img src="./resources/gorillaTechnologies.png" alt="" class="logo">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbardropdown">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
@@ -43,10 +47,11 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Total Price:100/-</a>
       </li>
+      
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
@@ -63,51 +68,85 @@
   </ul>
 </nav>
 
+
 <!-- third child -->
 <div class="bg-light">
   <h3 class="text-center">Gorilla Tech Store</h3>
-  <p class="text-center">Where we find affordable electronics tailored to your needs</p>
+  <p class="text-center">where we sell affordable electronics based on your needs</p>
 </div>
-
-
-
 
 <!-- fourth child -->
 <div class="row">
   <div class="col-md-10">
     <!-- products -->
     <div class="row">
-      <div class="col-md-4">
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-      </div>
-      <div class="col-md-4">
-      <div class="card" style="width: 18rem;">
+      <div class="col-md-4 mb-2">
+      <div class="card">
   <img src="./resources/intel quad-core N4120.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
   </div>
 </div>
       </div>
-      <div class="col-md-4">
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+      <div class="col-md-4 mb-2">
+      <div class="card">
+  <img src="./resources/intel quad-core N4120.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
   </div>
 </div>
       </div>
-    </div>
+      <div class="col-md-4 mb-2">
+      <div class="card">
+  <img src="./resources/intel quad-core N4120.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4 mb-2">
+      <div class="card">
+  <img src="./resources/intel quad-core N4120.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4 mb-2">
+      <div class="card">
+  <img src="./resources/intel quad-core N4120.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2">
+      <div class="card">
+  <img src="./resources/intel quad-core N4120.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to Cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+      </div>
+
   </div>
   <div class="col-md-2">
     <!-- sidenav -->
@@ -116,17 +155,30 @@
 </div>
 
 
+
+
+
+
 <!-- last child -->
-<!-- <div class="bg-dark p-3 text-center">
-    <p>All rights reserved</p>
+<!--<div class="bg-info p-3 text-center">
+  <p>All rights reserved</p>
 </div>-->
-    </div>
+  </div>
+  
 
-    
 
-<!-- boostrap js link -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+<!-- bootstrap js link -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" 
+integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" 
+integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+crossorigin="anonymous"></script>
 </body>
 </html>
